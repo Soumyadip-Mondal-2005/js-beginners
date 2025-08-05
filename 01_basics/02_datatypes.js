0584002007c3101afc1bf75905fff94712a96054
Soumyadip-Mondal-2005 <soumyadipar12@gmail.com>
2025-08-05 08:17:00 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+/*
+Types of DataTypes ---->
+number => 2^52
+bigint => large data
+boolean => true/false
+string =>""
+null => standalone value (object type)
+undefined => value (undefined type)
+*/
+let age = 25;
+let name = "Soumyadip";
+let isLoggedIn = true;
+
+console.log(typeof isLoggedIn);  //typeof operator used to see variable datatype.
